3b3d270c82463ecf9aa8cd0cc7123bd88a3523f8
Unknown <dragonofawsome@gmail.com>
2017-10-18 14:15:46 -0400

    Fixed bag images and randomly placed Skittles

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
 function setup() {
-    regular_bag = loadImage("./assets/skittles-regular-bag.png");
-    large_bag = loadImage("./assets/skittles-family-size.jpeg");
+    var candy_count = 20;
+    createCanvas(1000, 1000);
+    regular_bag = loadImage("https://raw.githubusercontent.com/dealien/Candy-Stats/master/assets/skittles-regular-bag.png");
+    large_bag = loadImage("https://raw.githubusercontent.com/dealien/Candy-Stats/master/assets/skittles-family-size.jpeg");
+    for (i = 0; i < candy_count; i++) {
+        x = Math.floor(Math.random() * 800) + 1
+        y = Math.floor(Math.random() * 350) + 251
+        ellipse(x, y, 50);
+    }
 }
 
 function draw() {
     //this function is called 60 times per second
-    //ellipse(x,y,width,height);
-    ellipse(30, 30, 50, 50);
-    image(regular_bag, 5, 5);
-    image(large_bag, , 415, 5);
+    image(regular_bag, 5, 5, 200, 160);
+    image(large_bag, 215, 5, 200, 200);
+
 }
