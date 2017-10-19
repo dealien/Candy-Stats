df1c0c98a97c31afa5c37e827c895de540253f87
Unknown <dragonofawsome@gmail.com>
2017-10-19 10:24:01 -0400

    Added Skittle generator

--- sketch.js
+++ sketch.js
@@ -1,18 +1,45 @@
 function setup() {
-    var candy_count = 20;
     createCanvas(1000, 1000);
     regular_bag = loadImage("https://raw.githubusercontent.com/dealien/Candy-Stats/master/assets/skittles-regular-bag.png");
     large_bag = loadImage("https://raw.githubusercontent.com/dealien/Candy-Stats/master/assets/skittles-family-size.jpeg");
-    for (i = 0; i < candy_count; i++) {
-        x = Math.floor(Math.random() * 800) + 1
-        y = Math.floor(Math.random() * 350) + 251
-        ellipse(x, y, 50);
-    }
+    frameRate(15);
+
 }
 
 function draw() {
-    //this function is called 60 times per second
     image(regular_bag, 5, 5, 200, 160);
     image(large_bag, 215, 5, 200, 200);
+    skittles_gen()
+}
 
+function skittles_gen() {
+    var candy_count = 20;
+    var green = color(140, 196, 94)
+    var red = color(196, 68, 68)
+    var yellow = color(255, 251, 15)
+    var orange = color(255, 170, 2)
+    var purple = color(90, 40, 112)
+
+    for (i = 0; i < candy_count; i++) {
+        x = Math.floor(Math.random() * 800) + 30;
+        y = Math.floor(Math.random() * 350) + 250;
+
+        chance = Math.random();
+        if (chance < 0.2459016393) {
+            fill(purple)
+        } else if (chance < 0.4098360656) {
+            fill(orange)
+        } else if (chance < 0.6721311475) {
+            fill(red)
+        } else if (chance < 0.8524590164) {
+            fill(green)
+        } else {
+            fill(yellow)
+        }
+
+        ellipse(x, y, 50);
+        fill(255, 255, 255);
+        textSize(32);
+        text("S", x - 11, y + 11);
+    }
 }
